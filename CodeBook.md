2efeb1b9d6a980606608c569ef5eb1985019b121
liubaochun <liubaochun@gmail.com>
2015-12-27 15:41:36 +0800

    Add comments in the script.

--- CodeBook.md
+++ CodeBook.md
@@ -106,7 +106,7 @@ Labels given from the original collectors were changed:
 From the intermediate data set is created a final tidy data set where numeric
 variables are averaged for each activity and each subject.
 
-The tidy data set contains 10299 observations with 81 variables divided in:
+The tidy data set contains 10299 observations with 68 variables divided in:
 
 *  an identifier of the subject who carried out the experiment (Subject):
    1, 3, 5, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25, 26, 27, 28, 29, 30
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 
+#Define the directories and files.
 rootDir <- file.path("./UCI HAR Dataset")
 testDir <- file.path(rootDir, "test")
 trainDir <- file.path(rootDir, "train")
@@ -12,9 +13,11 @@ trainXFile <- file.path(trainDir, "X_train.txt")
 trainYFile <- file.path(trainDir, "y_train.txt")
 trainSubjectFile <- file.path(trainDir, "subject_train.txt")
 
+#Read features.txt and activity_labels.txt
 features <- read.table(featuresFile)
 activities <- read.table(activitiesFile)
 
+#Merge the test and train data sets into one data set.
 testX <- read.table(testXFile)
 testY <- read.table(testYFile)
 testSubject <- read.table(testSubjectFile)
@@ -25,11 +28,14 @@ trainSubject <- read.table(trainSubjectFile)
 train <- cbind(trainSubject, trainY, trainX)
 dataSet <- rbind(test, train)
 
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
 columnNames <- c("Subject", "ActivityId", as.character(features[, 2]))
 grepIndex <- grep("Subject|ActivityId|-std\\(\\)|-mean\\(\\)", columnNames)
 columnNames <- make.names(names=columnNames, unique=TRUE, allow_=TRUE)
 names(dataSet) <- columnNames
 extractDataSet <- select(dataSet, grepIndex)
+
+#Labels the data set with descriptive variable names. 
 changeColName <- function(colName) {
     colName <- gsub("tBody", "Time.Body", colName)
     colName <- gsub("tGravity", "Time.Gravity", colName)
@@ -46,11 +52,15 @@ changeColName <- function(colName) {
     return(colName)
 }
 names(extractDataSet) <- changeColName(columnNames[grepIndex])
+
+#Uses descriptive activity names to name the activities in the data set
 names(activities) <- c("ActivityId", "ActivityName")
 tidyDataSet <- merge(extractDataSet, activities, by="ActivityId")
 tidyDataSet <- tidyDataSet[ , -which(names(tidyDataSet) %in% c("ActivityId"))]
 
+#Create an independent tidy data set with the average of each variable for each activity and each subject.
 activitySummary <- (tidyDataSet %>% group_by(Subject,ActivityName) %>% summarise_each(funs( mean)))
 
+#Output to files.
 write.csv(tidyDataSet, file = "tidyData.txt",row.names = FALSE)
 write.csv(activitySummary, file = "activitySummary.txt",row.names = FALSE)
